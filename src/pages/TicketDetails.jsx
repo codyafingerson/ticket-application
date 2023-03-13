5604339de6b3320cc98fc5a17c9bf8e246234682
Cody Fingerson <90158430+codyafingerson@users.noreply.github.com>
2023-03-13 12:54:15 -0600

    Configure redux to display selected ticket.

--- src/pages/TicketDetails.jsx
+++ src/pages/TicketDetails.jsx
@@ -4,6 +4,8 @@ import { useParams, useNavigate } from "react-router-dom";
 
 import { getTicketById } from "../features/tickets/ticketSlice";
 
+import Spinner from "../components/shared/Spinner";
+
 function TicketDetails() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -16,11 +18,14 @@ function TicketDetails() {
 
   useEffect(() => {
     dispatch(getTicketById(ticketId));
+    if (isError) alert(errorMessage);
   }, []);
 
   const goBack = () => {
     navigate(-1);
   };
+
+  if (isLoading) return <Spinner />;
 
   return (
     <div>
@@ -28,24 +33,45 @@ function TicketDetails() {
       <h1>{ticket.productName}</h1>
       <h2>{ticket.ticketNumber}</h2>
 
-      <div>
-        <h3>Product Details</h3>
-        {ticket.partInventory.map((part) => (
-          <div>
-            <p>{part.partNumber}</p>
-            <p>{part.description}</p>
-            <p>{part.currentLot}</p>
-          </div>
-        ))}
-      </div>
-      <div>
-        <h3>Notes</h3>
-        {ticket.notes.map((note) => (
-          <div>
-            <p><b>{note.createdBy}</b>: {note.body}</p>
-          </div>
-        ))}
-      </div>
+      {ticket && ticket.partInventory && (
+        <div>
+          <h3>Product Details</h3>
+          {ticket.partInventory.map((part) => (
+            <div key={part._id}>
+              <p>{part.partNumber}</p>
+              <p>{part.description}</p>
+              <p>{part.currentLot}</p>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {ticket && ticket.notes && (
+        <div>
+          <h3>Notes</h3>
+          {ticket.notes.map((note) => (
+            <div key={note._id}>
+              <p>
+                <b>{note.createdBy}</b>: {note.body}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+
+      {ticket.stationRecords && ticket.stationRecords.length > 0 && (
+        <div>
+          <h3>Stations</h3>
+          {ticket.stationRecords.map((s) => (
+            <div key={s._id}>
+              <p>{s.station}</p>
+              <p>{s.completedBy}</p>
+              <p>{s.timeTaken}</p>
+              <p>{new Date(s.dateCompleted).toLocaleDateString('en-US')}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
